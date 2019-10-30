fef8b1b13df1fefa07efce0ba7cf11cd6d6cdbde
Nikhil Sangwan <nikhil.sangwan95@gmail.com>
2019-10-30 22:08:28 +0530

    create idsssigning method

--- main.cpp
+++ main.cpp
@@ -3,20 +3,28 @@
 
 using namespace std;
 
-int main()
-{
-    int numberOfNodes;
-	// cout<<"Enter number of nodes : ";
-	cin>>numberOfNodes;
+void IDAssigningMethod(int numberOfNodes){
 	vector<Node> nodeVector;
 
-	//setting ID to nodes
 	for(int i=0; i<numberOfNodes; ++i){
 		Node node;
 		node.setID(i);
 		nodeVector.push_back(node);
 	}
+
 	for(Node n : nodeVector)
 		cout<<n.getID()<<" ";
+	cout<<endl;
+}
+
+int main()
+{
+    int numberOfNodes;
+	cout<<"Enter number of nodes : ";
+	cin>>numberOfNodes;
+
+	//setting ID to nodes
+	IDAssigningMethod(numberOfNodes);
+	
     return 0;
 }
